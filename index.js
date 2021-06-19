798f68a679d14f11612479b9b64429506478edad
SiarheiKarol <m.jjesire@gmail.com>
2021-06-19 15:35:48 +0300

    add:name-to-print

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ async function modifyPdf() {
   // Get the first page of the document
   const pages = pdfDoc.getPages()
   const firstPage = pages[0]
-  const textToPrint = document.getElementById("textToPrint").value
+  const textToPrint = document.getElementById("name").value
   // Get the width and height of the first page
   const { width, height } = firstPage.getSize()
 
