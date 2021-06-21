aab1fbd5006fdd3e61837804f628b65e8e74231a
SiarheiKarol <m.jjesire@gmail.com>
2021-06-21 14:26:04 +0300

    new

--- index.js
+++ index.js
@@ -137,7 +137,7 @@ async function modifyPdf() {
   })
 
   firstPage.drawText(second_name, {
-    x: 804 - second_name.length,
+    x: 804 - second_name.length*4,
     y: 527,
     size: 9,
     font: helveticaFont,
