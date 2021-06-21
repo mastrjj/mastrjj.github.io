57cf5f8ae2fbdbe8eed82086a91db31303c1d9f0
SiarheiKarol <m.jjesire@gmail.com>
2021-06-21 16:54:44 +0300

    new

--- index.js
+++ index.js
@@ -45,8 +45,9 @@ async function modifyPdf() {
   const f_num = document.getElementById("num_f").value
   const card_num = document.getElementById("num_card").value
   const arrival_datetime = new Date(document.getElementById("date").value)
+  const payment_datetime = new Date(arrival_datetime.getTime() + getRandomIntInclusive(8,15) * 60 * 1000)
+  console.log(payment_datetime.toLocaleTimeString())
   const departure_datetime = new Date(arrival_datetime.getTime() + 60 * 60 * 24 * 1000)
-  console.log(departure_datetime.toLocaleDateString('en-GB'))
   const p_date = arrival_datetime.toLocaleDateString('en-GB')
   const p_time = arrival_datetime.toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'})
   const randString8 = getRandomString(8)
