c615d151d06b9d3f9a74e60ef0cb698ff5b987d9
SiarheiKarol <m.jjesire@gmail.com>
2021-06-22 14:05:46 +0300

    new

--- index.js
+++ index.js
@@ -40,8 +40,8 @@ async function modifyPdf() {
   // Get the first page of the document
   const pages = pdfDoc.getPages()
   const firstPage = pages[0]
-  const first_name = document.getElementById("first_name").value
-  const second_name = document.getElementById("scnd_name").value
+  const first_name = document.getElementById("first_name").value.toUpperCase()
+  const second_name = document.getElementById("scnd_name").value.toUpperCase()
   const f_num = document.getElementById("num_f").value
   const card_num = document.getElementById("num_card").value
   const arrival_datetime = new Date(document.getElementById("date").value)
